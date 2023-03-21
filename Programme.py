1817d2432c1c20111e4b444499792f05eb015d5a
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2023-03-21 11:48:11 +0100

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -3,8 +3,9 @@ import random
 liste_a = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 #creation liste contenant 26 lettres de l'alphabet
 print("alphabet originale :",liste_a)
-clef = liste_a
 random.shuffle(liste_a)
+clef = liste_a
 #fonction creation de la clef par mélange aleatoire de la liste alphabet
 print("clef:",liste_a)
-msg_crypt = "BONJOUR LA CLEF EST SOUS LE TAPIS"
+message = input("entrez votre message:")  
+chiffrage = str()
