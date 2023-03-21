3cc955af28d67a66b588a8ecee80ab40d6e7afbd
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2023-03-21 10:54:23 +0100

    Update Programme.py

---
+++ Programme.py
@@ -0,0 +1,6 @@
+#marie_subst_monoalphabetique
+import random 
+liste_a = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+print("alphabet originale :",liste_a)
+random.shuffle(liste_a)
+print("clef:",liste_a)
