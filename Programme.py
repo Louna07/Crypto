40d02345c4b843ce00c3676eb731da71eaf7793a
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2023-03-21 10:58:39 +0100

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -1,6 +1,8 @@
 #marie_subst_monoalphabetique
 import random 
 liste_a = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+#creation liste contenant 26 lettres de l'alphabet
 print("alphabet originale :",liste_a)
 random.shuffle(liste_a)
+#fonction creation de la clef par mélange aleatoire de la liste alphabet
 print("clef:",liste_a)
