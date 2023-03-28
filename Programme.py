0761227451314f532a906a96adb0fc2eadae4fff
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2023-03-28 16:45:42 +0200

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -1,11 +1,14 @@
 #marie_subst_monoalphabetique
 import random 
 liste_a = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-#creation liste contenant 26 lettres de l'alphabet
+#création liste contenant 26 lettres de l'alphabet
 print("alphabet originale :",liste_a)
 random.shuffle(liste_a)
 clef = liste_a
-#fonction creation de la clef par mélange aleatoire de la liste alphabet
+#variable création de la clef par mélange aleatoire de la liste alphabet
 print("clef:",liste_a)
 message = input("entrez votre message:")  
+#ici on veut renvoyer une chaine de caractére pour le chiffrage
 chiffrage = str()
+#on parcoure chaque lettre de notre message avec la fonction
+for lettre in message: 
