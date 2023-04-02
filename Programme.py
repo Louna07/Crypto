c59be5ffa3ea5ce65da92b0fd754ad3ecbb4c63f
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2023-04-02 13:23:36 +0200

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -1,14 +1,26 @@
-#marie_subst_monoalphabetique
-import random 
-liste_a = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-#création liste contenant 26 lettres de l'alphabet
-print("alphabet originale :",liste_a)
-random.shuffle(liste_a)
-clef = liste_a
-#variable création de la clef par mélange aleatoire de la liste alphabet
-print("clef:",liste_a)
-message = input("entrez votre message:")  
-#ici on veut renvoyer une chaine de caractére pour le chiffrage
-chiffrage = str()
-#on parcoure chaque lettre de notre message avec la fonction
-for lettre in message: 
+#ici on a chiffrer un message par le technique de substituion mono-alphabétique.MARIE-MICHELE1
+import random
+#definition de l'alphabet
+alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print("alphabet original:",alphabet)
+#création de notre clè de substitution 
+substitution = list(alphabet)
+random.shuffle(substitution)
+print("nouvel alphabet:",substitution)
+message = input("entrez votre message en majuscule :")
+#definir la fonction qui va chiffrer le message
+def chiffre_message(message):
+    #initialisation du message chiffré
+    message_chiffre = ""
+    #on chiffre chaque caractere du message
+    for caractere in message:
+        if caractere in alphabet:
+            indice = alphabet.index(caractere)
+            caractere_chiffre = substitution[indice]
+            message_chiffre += caractere_chiffre
+        else:
+            message_chiffre += caractere
+    return message_chiffre
+message_chiffre = chiffre_message(message)
+print("message original:",message)
+print("message chiffré:",message_chiffre)
