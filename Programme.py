87770970b01738360fbdddc26e28295f693c722a
uvsq22103840 <114145374+uvsq22103840@users.noreply.github.com>
2023-04-03 22:10:09 +0200

    Update Programme.py

--- Programme.py
+++ Programme.py
@@ -1,42 +1,33 @@
-#ici on a chiffrer un message par le technique de substituion mono-alphabétique.MARIE-MICHELE1
+#Chiffrement par substitution mono-alphabétique. MARIE-MICHELE1
 import random
-import tkinter as tk
-HAUTEUR = 600
-LARGEUR = 600
-#definition de l'alphabet
-alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-print("alphabet original:",alphabet)
+
+#création de l'alphabet
+liste_a = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print("alphabet original:",liste_a)
+
 #cle de substitution 
-clef = list(alphabet)
+clef = list(liste_a)
 random.shuffle(clef)
 print("nouvel alphabet:",clef)
-message = input("entrez votre message :")
+message_en_clair = input("entrez votre message :")
 
-#definir une fonction qui va chiffrer le message
-def chiffre_message(message):
-    #initialisation d'une variable qui contient le message chiffré 
-    message_chiffre = ""
-    #on chiffre chaque caractere du message, on vérifie que chaque carctere est dans l'alphabaet.
-    #on determine l'indice de ce caractere dans la liste alphabet grace a index()
+#création de la fonction qui va chiffrer notre message
+def chiffré_message(message_en_clair):
+    #on créé une variable qui contiendra notre texte chiffré 
+    message_chiffré = ""
+    #on vérifie que chaque lettre ou caractere est dans l'alphabaet pré-définie.
+    #on determine la position de la lettre dans la liste alphabet grace a index().on uttilise la position de la lettre dans la liste pour trouver la lettre correspondante dans l'alphabet de substitution
+    #si la lettre ou le caractere n'est pas definie, celui ci est directemet renvoyer dans le message chiffré tels quel.
     #la fonction renvoie la variable message chiffre.
-    for caractere in message:
-        if caractere in alphabet:
-            indice = alphabet.index(caractere)
-            caractere_chiffre = clef[indice]
-            message_chiffre += caractere_chiffre
+    for lettre in message_en_clair:
+        if lettre in liste_a:
+            position = liste_a.index(lettre)
+            lettre_chiffré = clef[position]
+            message_chiffré += lettre_chiffré
         else:
-            message_chiffre += caractere
-    return message_chiffre
-
-message_chiffre = chiffre_message(message)
-print("message original:",message)
-print("message chiffré:",message_chiffre)
-
-#décryptage d'un message avec clef connue. 
-clef = list("MNLAVRBOZJKYDSWHEFCXTUPQGI")
-
-racine =tk.Tk()
-racine.title("")
-canvas=tk.Canvas(racine, width=LARGEUR, height=HAUTEUR)
-
-racine.mainloop()
+            message_chiffré += lettre
+    return message_chiffré
+#on appelle la fonction dans notre variable
+message_chiffré = chiffré_message(message_en_clair)
+print("message original:",message_en_clair)
+print("message chiffré:",message_chiffré)
